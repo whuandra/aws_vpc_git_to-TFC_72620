e164d8275dc28798472d402a68516f21a5dae7a8
whuandra <50607920+whuandra@users.noreply.github.com>
2020-07-29 20:24:22 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variable "aws_access_key" {}
+variable "aws_access_key_id" {}
 variable "aws_secret_key" {}
 
-#variable "aws_access_key" {
+#variable "aws_secret_access_key" {
 #     description = "Access key to AWS console"
 #     
 #}

6ef718d781e3f65cf41e263ba66b81711cd75bbe
whuandra <50607920+whuandra@users.noreply.github.com>
2020-07-30 20:26:36 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,8 +3,8 @@ provider "aws" {
     #access_key  = var.aws_access_key_id
     #secret_key  = var.aws_secret_access_key
     
-    access_key = "${var.AWS_ACCESS_KEY_ID}"
-    secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+    aws_access_key_id = var.aws_access_key_id
+    aws_secret_access_key = var.aws_secret_access_key
 
 }
 

9dd5123b412458238f934e561959c6fbd92920f4
whuandra <50607920+whuandra@users.noreply.github.com>
2020-07-31 20:54:07 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 
 resource "aws_vpc" "default" {
-  #cidr_block           = var.cidr_block
+  cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
 }

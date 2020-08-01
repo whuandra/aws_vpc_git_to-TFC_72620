5676fd857ab80e81830a0d3328e70b90d4a97290
whuandra <50607920+whuandra@users.noreply.github.com>
2020-07-31 20:41:01 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,12 +8,10 @@ provider "aws" {
 resource "aws_vpc" "default" {
     cidr_block = var.cidr_block
     enable_dns_support = true
-    enable_dns_hostname = true
+    enable_dns_hostnames = true
 }
 
-resource "aws_internet_gateway" "default"{
-    vpc_id = aws_vpc.default.id
-}
+
 
 
 resource "aws_route_table" "private" {
@@ -72,6 +70,10 @@ resource "aws_route_table_association" "public" {
 
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_internet_gateway" "default"{
+    vpc_id = aws_vpc.default.id
 }
     
 /*terraform {

90a26db3e86ae7566942b66124486c7fb031e7b9
whuandra <50607920+whuandra@users.noreply.github.com>
2020-08-01 20:09:52 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,55 +11,19 @@ variable "cidr_block" {
 }
 
 variable "private_subnet_cidr_blocks" {
-  default     = ["10.0.1.0/24", "10.0.3.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
   type        = list
   description = "List of private subnet CIDR blocks"
-}
-
-
-/*variable "aws_secret_access_key" {
-  type = string
-  }
-variable "aws_access_key_id" {
-  type = string
-  }
-
-#variable "aws_secret_access_key" {
-#     description = "Access key to AWS console"
-#     
-#}
-#variable "aws_secret_access_key" {
-#     description = "Secret key to AWS console"
-#     
-#}
-
-variable "region" {
-  default     = "us-east-1"
-  type        = string
-  description = "Region of the VPC"
-}
-
-
-variable "cidr_block" {
-  default     = "10.0.0.0/16"
-  type        = string
-  description = "CIDR block for the VPC"
 }
 
 variable "public_subnet_cidr_blocks" {
-  default     = ["10.0.0.0/24", "10.0.2.0/24"]
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
   type        = list
   description = "List of public subnet CIDR blocks"
-}
-
-variable "private_subnet_cidr_blocks" {
-  default     = ["10.0.1.0/24", "10.0.3.0/24"]
-  type        = list
-  description = "List of private subnet CIDR blocks"
 }
 
 variable "availability_zones" {
   default     = ["us-east-1a", "us-east-1b"]
   type        = list
   description = "List of availability zones"
-}*/
+}
